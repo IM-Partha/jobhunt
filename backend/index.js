1e3058475c426f8c0d79e78a503a8bd08cb4a9d3
Partha Debnath <85936227+IM-Partha@users.noreply.github.com>
2025-11-04 22:02:08 +0530

    url remove

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,12 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import connectDB from './utils/db.js';
-import userRouter from './routes/user.routes.js';
-import comapnyRouter from './routes/company.route.js';
-import jobRouter from './routes/job.route.js';
-import applicationRouter from './routes/application.route.js';
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+import userRouter from "./routes/user.routes.js";
+import comapnyRouter from "./routes/company.route.js";
+import jobRouter from "./routes/job.route.js";
+import applicationRouter from "./routes/application.route.js";
 
 dotenv.config();
 
@@ -18,22 +18,22 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: "http://localhost:5173",
   credentials: true,
 };
 app.use(cors(corsOptions));
 
 // routes
-app.use('/api/v1/user', userRouter);
-app.use('/api/v1/company', comapnyRouter);
-app.use('/api/v1/job', jobRouter);
-app.use('/api/v1/application', applicationRouter);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/company", comapnyRouter);
+app.use("/api/v1/job", jobRouter);
+app.use("/api/v1/application", applicationRouter);
 
-app.get('/',(req,res)=>{
+app.get("/", (req, res) => {
   return res.status(200).json({
-    message:"pong"
-  })
-})
+    message: "pong",
+  });
+});
 // server
 const PORT = process.env.PORT || 3000;
 
